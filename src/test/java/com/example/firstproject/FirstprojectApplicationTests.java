e1d48893f59381118f39aa9f4b34ce25519c0dd7
JiHyeon Yun <heon__y@naver.com>
2025-05-07 21:39:32 +0900

    하드코딩 삭제

--- src/test/java/com/example/firstproject/FirstprojectApplicationTests.java
+++ src/test/java/com/example/firstproject/FirstprojectApplicationTests.java
@@ -1,13 +1,8 @@
 package com.example.firstproject;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class FirstprojectApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
